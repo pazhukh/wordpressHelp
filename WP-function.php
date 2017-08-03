25fa7b997c7b294c51f1185f5e14fdac551ffd6e
pazhukh <pazhukh@gmail.com>
2017-08-03 11:10:40 +0300

    Update WP-function.php

--- WP-function.php
+++ WP-function.php
@@ -14,10 +14,13 @@ register_nav_menus( array(
 //реєструємо наші скріпти в футер та стилі
 function theme_name_scripts() {
         //реєструємо наш jquery (відміняємо jquery worpdress)
-  wp_deregister_script( 'jquery' );
+  	wp_deregister_script( 'jquery' );
 	wp_register_script('jquery', get_template_directory_uri() . '/js/jquery-3.1.1.min.js', '','', true);
 	wp_enqueue_script( 'jquery' );
-  
+  	
+	//підключаємо jquery який є в wordpress
+	wp_enqueue_script( 'jquery' );
+
 	wp_enqueue_script('googleMap', 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAowdFNkUGZ-b6WvxwkyII2Lry2hjO6vcY', '', '', true);
 	wp_enqueue_script('script', get_template_directory_uri() . '/js/common.js', '','', true);
   
@@ -26,7 +29,7 @@ function theme_name_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'theme_name_scripts' );
 
-
+jQuery(function ($) {}
 
 //thumbnails in posts
 add_theme_support('post-thumbnails'); 
