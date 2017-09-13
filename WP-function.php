a9f7cddeb4eac8028f0d0e1576d857088ff03301
pazhukh <pazhukh@gmail.com>
2017-09-13 13:47:42 +0300

    Update WP-function.php

--- WP-function.php
+++ WP-function.php
@@ -363,3 +363,23 @@ return $content;
 }
 add_action('navigation_markup_template', 'sanitize_pagination');
 	
+*****************************************************************************************************
+	
+//hide Wysiwyg Editor on page ID 2024
+add_action( 'admin_init', 'hide_editor' );
+function hide_editor() {
+  // Get the Post ID.
+  $post_id = $_GET['post'] ? $_GET['post'] : $_POST['post_ID'] ;
+  if( !isset( $post_id ) ) return;
+  // Hide the editor on the page titled 'Homepage'
+  $homepgname = $post_id;
+  if($homepgname == 2024){ 
+    remove_post_type_support('page', 'editor');
+  }
+  // Hide the editor on a page with a specific page template
+  // Get the name of the Page Template file.
+  $template_file = get_post_meta($post_id, '_wp_page_template', true);
+  if($template_file == 'my-page-template.php'){ // the filename of the page template
+    remove_post_type_support('page', 'editor');
+  }
+}
