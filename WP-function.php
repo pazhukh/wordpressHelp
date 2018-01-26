8e4c269c4ada35621e5c758b7d9e59bcba0ee227
pazhukh <pazhukh@gmail.com>
2018-01-26 16:58:59 +0200

    Update WP-function.php

--- WP-function.php
+++ WP-function.php
@@ -1,8 +1,23 @@
 // delete trash
-remove_action ('wp_head', 'rsd_link');
-remove_theme_support ('automatic-feed-links');
-remove_action ('wp_head', 'wlwmanifest_link');
-remove_action ('wp_head', 'wp_generator');
+if ( ! function_exists('cleanup_head') ) {
+  function cleanup_head() {
+    remove_action('wp_head', 'wp_generator');
+    remove_action('wp_head', 'rsd_link');
+    remove_action('wp_head', 'wlwmanifest_link');
+    remove_action('wp_head', 'index_rel_link');
+    remove_action('wp_head', 'feed_links', 2);
+    remove_action('wp_head', 'feed_links_extra', 3);
+    remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0);
+    remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0);
+    remove_action('wp_head', 'print_emoji_detection_script', 7);
+    remove_action('wp_print_styles', 'print_emoji_styles');
+    remove_theme_support ('automatic-feed-links');
+  }
+}
+add_action('init', 'cleanup_head');
+
+
+
 show_admin_bar(false);
 
 ********************************************************************  
